7311f77ef625ab106deef8335e41a91779fdbfd6
Bruno Franco <11.brunofm@gmail.com>
2021-06-28 16:44:40 -0300

    Finalização da autenticação com context e hook

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -6,8 +6,11 @@ import logoImg from '../assets/images/logo.svg';
 import '../styles/auth.scss';
 
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 export function NewRoom() {
+    const { user } = useAuth();
+
     return(
         <div id="page-auth">
             <aside>
@@ -18,6 +21,8 @@ export function NewRoom() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
+                    <img src={user?.avatar} alt="Avatar" className="avatar" />
+                    <h1> {user?.name} </h1>
                     <h2> Criar uma nova sala </h2>
                     <form action="">
                         <input type="text" placeholder="Nome da sala" />
